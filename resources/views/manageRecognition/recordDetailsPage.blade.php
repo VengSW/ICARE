0a35ce676ee39de323a6f214814b5daf2fa8bc2b
VengSW <vengsumw@gmail.com>
2021-09-19 14:59:02 +0800

    Create recordDetailsPage.blade.php

---
+++ resources/views/manageRecognition/recordDetailsPage.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.main')
+
+@section('content')
+<?php	
+	session_start();
+	
+?>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Record Page') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+                    this is recognition page
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
