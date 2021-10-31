7c59192e7bc0ac7dea9bc4f23c89b43da006560b
VengSW <vengsumw@gmail.com>
2021-10-31 21:24:48 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Fascades\Mail;
+use Illuminate\Support\Fascades\Session;
 
+use App\Http\Controllers\ManageRecognitionController;
+use App\Http\Controllers\ManageExerciseController;
+use App\Http\Controllers\ManageTimerController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\SessionController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -23,36 +29,87 @@ Auth::routes(['verify' => true]);//verification mail
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-//------------------MANAGE REGISTRATION------------------------------
+//----------------------------------------------------------------------------MANAGE REGISTRATION------------------------------
 //route for register account
-Route::get('/manageRegistration','ManageRegistrationController@index');
+
 
 //Redirect to homepage
-Route::get('/homepage', function () {
-    return view('manageRegistration/homepage');
-});
+/*Route::get('/homepage', function () {
+    return view('ManageRegistration/homepage');
+});*/
+
 //Redirect to learn more page
 Route::get('/learnmore', function () {
-    return view('manageRegistration/learnmore');
+    return view('ManageRegistration/learnmore');
 });
 
-//Redirect to recognition page
-Route::get('/recognition', function () {
-    return view('manageRecognition/recognitionPage');
-});
-//Redirect to recognition started page
-Route::get('/recognitionStarted', function () {
+
+/**/
+//------------------------------------------------------------------------------MANAGE RECOGNITION------------------------------------------------------------------------
+//Redirect to recognition page successfully
+/*Route::get('/recognition', function () { //works
+    return view('ManageRecognition/recognitionPage');
+});*/
+Route::post('submit','App\Http\Controllers\ManageRecognitionController@create')->name('ManageRecognition.create');//Create record successfully
+
+
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<STILL WORKING ON IT>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+Route::get('/data', [SessionController::class,'data']);
+Route::get('/try', [SessionController::class,'try']);
+Route::get('/try3', [SessionController::class,'try3']);
+Route::get('/try2/{id}',[SessionController::class,'try2']);
+
+//Route::get('/',[SessionController::class,'getLogout']);
+
+
+
+Route::get('/recognition/{id}',[ManageRecognitionController::class,'show'])->name('ManageRecognition.show');
+
+
+
+
+
+//test view recognition page with viewRecognitionPage()
+Route::get('/ManageRecognition/viewRecognitionPage/{id}','App\Http\Controllers\ManageRecognitionController@viewRecognitionPage')->name('ManageRecognition.viewRecognitionPage');//missing parameter
+
+
+
+//redirect to recognition started page
+Route::get('/startRecognition', function () {//works
     return view('manageRecognition/recognitionStartedPage');
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 //Redirect to record details page
 Route::get('/record', function () {
-    return view('manageRecognition/recordDetailsPage');
+    return view('ManageRecognition/recordDetailsPage');
 });
 //Redirect to exercises page
 Route::get('/exercise', function () {
-    return view('manageExercise/exercisePage');
+    return view('ManageExercise/exercisePage');
 });
 //Redirect to timer page
 Route::get('/timer', function () {
-    return view('manageTimer/timerSettingPage');
+    return view('ManageTimer/timerSettingPage');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
