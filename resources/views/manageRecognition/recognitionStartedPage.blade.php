bf0e5a4f73b572541c0f81701d8d719fd38ea250
VengSW <vengsumw@gmail.com>
2021-10-31 21:24:58 +0800

    Update recognitionStartedPage.blade.php

--- resources/views/manageRecognition/recognitionStartedPage.blade.php
+++ resources/views/manageRecognition/recognitionStartedPage.blade.php
@@ -1,28 +1,34 @@
 @extends('layouts.main')
 
 @section('content')
-<?php	
-	session_start();
-	
-?>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Recognition Started Page') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                    this is recognition page
-                </div>
             </div>
         </div>
     </div>
 </div>
+<form action="submit" method="post">
+    @csrf
+    <center>
+    <table>
+        <input type="hidden" name="recordid" >
+        <input type="hidden" name="userid" value="1000">
+        <tr>
+            <td><input type="date" name="date" placeholder="date" ></td>
+        </tr>
+        <tr>
+            <td><label for="img">Select image:</label></td>
+        </tr>
+        <tr>
+            <td><input type="text" id="img" name="image"></td>
+        </tr>
+        <tr>
+            <td><input type="submit"></td>
+        </tr>
+    </table>
+</center>
+</form>
 @endsection
