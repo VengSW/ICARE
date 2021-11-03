2e3530e609321d448efc48e6379860790823ce3d
VengSW <vengsumw@gmail.com>
2021-11-03 16:55:38 +0800

    Create 2021_10_27_042901_create_record_table.php

---
+++ database/migrations/2021_10_27_042901_create_record_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRecordTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('records', function (Blueprint $table) {
+            $table->id('recordID')->startingValue(1000);
+            $table->string('UserID');
+            $table->date('date');
+            $table->binary('image');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('records');
+    }
+}
