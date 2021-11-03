43036c8face7488cd70e8189b9304cf285e5a71a
VengSW <vengsumw@gmail.com>
2021-11-04 02:11:42 +0800

    Create nav.blade.php
    Updated navigation bar of the system

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,227 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <title>Collapsible sidebar using Bootstrap 4</title>
+
+    <!-- Bootstrap CSS CDN -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <!-- Our Custom CSS -->
+    <link rel="stylesheet" href="style.css">
+
+    <!-- Font Awesome JS -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+
+    <style>
+        .wrapper {
+            display: flex;
+            align-items: stretch; /*height of navbar influenced by content*/
+        }
+
+        #sidebar {
+            min-width: 250px;
+            max-width: 250px;
+            min-height: 100vh;
+        }
+
+        #sidebar.active {
+            margin-left: -250px;
+        }
+
+        a[data-toggle="collapse"] {
+            position: relative;
+        }
+
+        .dropdown-toggle::after {
+            display: block;
+            position: absolute;
+            top: 50%;
+            right: 20px;
+            transform: translateY(-50%);
+        }
+        /*Media query for customizing sidebar behaviour for smaller screens*/
+        @media (max-width: 768px) {
+            #sidebar {
+                margin-left: -250px;
+            }
+            #sidebar.active {
+                margin-left: 0;
+            }
+        }
+
+        /*Additional styling*/
+        body {
+            font-family: 'Poppins', sans-serif;
+            background: #FEFFDE;
+        }
+
+        p {
+            font-family: 'Poppins', sans-serif;
+            font-size: 1.1em;
+            font-weight: 300;
+            line-height: 1.7em;
+            color: #999;
+        }
+
+        a, a:hover, a:focus {
+            color: inherit;
+            text-decoration: none;
+            transition: all 0.3s;
+        }
+
+        #sidebar {
+            /* don't forget to add all the previously mentioned styles here too */
+            background: #7C9473;/*7386D5*/
+            color: #fff;
+            transition: all 0.3s;
+        }
+
+        #sidebar .sidebar-header { /*sidebar menu title*/
+            padding: 20px;
+            background: #CFDAC8;/*6d7fcc*/
+            color:darkolivegreen;
+            text-align: center;
+            font-family: "Arcena";
+        }
+
+        #sidebar ul.components { /*underline*/
+            padding: 20px 0;
+            border-bottom: 1px solid #7C9473;/*47748b*/
+        }
+
+        #sidebar ul p { /*sidebar name*/
+            color: #fff;
+            padding: 10px;
+            font-family: "Arcena";
+            font-size: 30px;
+        }
+
+        #sidebar ul li a {/*sidebar content width*/
+            padding: 10px;
+            font-size: 1.1em;
+            display: block;
+        }
+        #sidebar ul li a:hover {/*sidebar content hover*/
+            color: #C7CFB7;/*7386D5*/
+            background: #fff;
+        }
+
+        #sidebar ul li.active > a, a[aria-expanded="true"] { /*current active page*/
+            color: #fff;
+            background: #ADC2A9;/*6d7fcc*/
+        }
+        ul ul a { /*dropdown content*/
+            font-size: 0.9em !important;
+            padding-left: 30px !important;
+            background: #E8EAE6;/*6d7fcc*/
+            color:darkolivegreen;
+        }
+
+        #content, h2{
+            margin: 10px;
+            font-family: "Lucida Fax";
+            color: #52734D;
+        }
+    </style>
+</head>
+        
+<body>
+    
+
+    <div class="wrapper">
+        <!-- Sidebar -->
+        <nav id="sidebar">
+            <div class="sidebar-header">
+                <h3>ICARE</h3>
+            </div>
+    
+            <ul class="list-unstyled components">
+                <p id="username" >{{ Auth::user()->name }}</p>
+                <li class="active">
+                    <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Home</a> <!--data-toggle for the dropdown effect; droptown-toggle: arrow icon-->
+                    <ul class="collapse list-unstyled" id="homeSubmenu">
+                        <li>
+                            <a href="#">Profile</a>
+                        </li>
+                        <li>
+                            <a href="home">Home</a>
+                        </li>
+                        <li>
+                        <a href="{{route('logout') }}" onclick="event.preventDefault(); 
+                            document.getElementById('logout-form').submit(); alert('You have logout!!');">{{ __('Logout') }}</a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </li>
+                    </ul>
+                </li>
+                
+                <li>
+                    <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Exercise</a>
+                    <ul class="collapse list-unstyled" id="pageSubmenu">
+                        <li>
+                            <a href="exercise">Page 1</a>
+                        </li>
+                        <li>
+                            <a href="#">Page 2</a>
+                        </li>
+                        <li>
+                            <a href="#">Page 3</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="recognition">Recognition</a>
+                </li>
+                <li>
+                    <a href="timer">Timer</a>
+                </li>
+                
+                <li>
+                    <a href="learnmore">About</a>
+                </li>
+            </ul>
+        </nav>
+        
+        <!--SIDE TOGGLE BUTTON-->
+        <br>
+    <div id="content">
+        <!-- <nav class="navbar navbar-expand-sm navbar-light bg-light"> -->
+            <div class="container-fluid">
+                <button type="button" id="sidebarCollapse" class="btn" style="background-color: #7C9473;color: white;" >
+                    <i class="fas fa-align-left"></i><!--Menu button icon-->
+                </button>
+            </div>
+    </div>
+        <h2>Welcome Back!!</h2>
+        <!-- Your Content Here-->
+        <div class="container">
+            @yield('content')
+        </div>
+    </div>
+    
+    <!-- jQuery CDN - Slim version (=without AJAX) -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <!-- Popper.JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+
+    <!--Script for the sidebar animation -->
+    <script>
+        $(document).ready(function () {
+            $('#sidebarCollapse').on('click', function () {
+                $('#sidebar').toggleClass('active');
+            });
+        });
+    </script>
+
+    
+</body>
+
+</html>
