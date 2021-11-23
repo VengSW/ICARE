ee6989edb83f492b92ee481913fa8dfa5a9bbae2
VengSW <vengsumw@gmail.com>
2021-11-23 13:10:21 +0800

    Update recordDetailsPage.blade.php

--- resources/views/manageRecognition/recordDetailsPage.blade.php
+++ resources/views/manageRecognition/recordDetailsPage.blade.php
@@ -1,28 +1,60 @@
-@extends('layouts.main')
+@extends('layouts.nav')
 
 @section('content')
-<?php	
-	session_start();
-	
-?>
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Record Page') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    {{ $data }}
-                    {{ __('You are logged in!') }}
-                    this is recognition page
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+<!-- {{ $data }} -->
+<head>
+<style>
+        @import url('https://fonts.google.com/specimen/ZCOOL+XiaoWei?preview.text=Button&preview.text_type=custom#standard-styles');
+
+        .card{
+            margin: 10px 100px;
+        }
+        table{
+            width: 125%;
+            margin: 70px;
+            font-size: 15px;
+            color: white;
+            border-color: white;
+            justify-content: center;
+        }
+        th{
+            background-color: #5c7a57;
+            text-align: center;
+        }
+        td{
+            background-color: #759c6e;
+            text-align: center;
+        }
+
+        button{
+            font-family: "Lucida Fax";
+            font-size: 15px;
+            color: white;
+            border: none;
+            align-items: center;
+            background-color: #759c6e;
+        }
+        button:hover{
+            background-color: #77a66f;
+        }
+    </style>
+</head>
+<h2 class="header">{{ __('Record Details Page') }}</h2>
+    <p>This is your record of poor sitting posture.</p>
+    <center>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>No.</th><td>{{ $data->recordID}}</td>
+            </tr>
+            <tr>
+                <th>Date & Time</th><td>{{ $data->created_at}}</td>
+            </tr>
+            <tr style="height=50vh;">
+                <th>Image</th><td>{{ $data->Image}}</td>
+            </tr>
+        </thead>
+    </table>
+
 @endsection
