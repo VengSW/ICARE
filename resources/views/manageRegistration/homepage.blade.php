e629e1e04aecaee0cc53bfc6ca6e49a1b649db7e
VengSW <vengsumw@gmail.com>
2021-11-29 00:36:20 +0800

    Update homepage.blade.php

--- resources/views/manageRegistration/homepage.blade.php
+++ resources/views/manageRegistration/homepage.blade.php
@@ -7,36 +7,78 @@
 ?>
 <style>
     .content{
-        margin: 10px 100px;
+        transform: translateX(30px);
     }
     .card{
         background-color: #7C9473 ;
         color:white;
-        transform: translateX(400px) translateY(100px);
+        transform: translateX(430px) translateY(50px);
     }
     .img{
         width: 200px;
         height:200px;
     }
+    button{
+        font-family: "Lucida Fax";
+        font-size: 15px;
+        color: white;
+        border: none;
+        align-items: center;
+        background-color: #597854;
+        border-radius: 5px;
+    }
+    button:hover{
+        background-color: #677a5f;
+    }
+    ::-webkit-input-placeholder {
+        color:white;
+    }
+    input{
+        color:white;
+    }
 </style>
 <title>Home</title>
 <body>
-<h2 class="header">Welcome back  {{ Auth::user()->name }}</h2>
-<!-- <div class="content">
+<p>@if(session()->get('success'))
+      {{ session()->get('success') }}  
+<br></p>
+@endif
+<h2 class="header" style="transform: translateX(650px);">Home Page</h2>
+<!-- <img id="logo" src="/images/icare2.png" alt="logo" width="150" height="150" style="transform: translateX(700px);"> -->
+<div class="content">
 <div class="container">
     <div class="row justify-content-center">
         <div class=".col-xs-12 .col-sm-6 .col-lg-8">
             <div class="card shadow">
-            <div class="navbar-brand" style="font-size: 40px; color:white; ">Welcome back  {{ Auth::user()->name }}</div><br> 
-                <div class="card-header">{{ __('Dashboard') }}</div>
+            <div class="navbar-brand" style="font-size: 40px; color:white; text-align:center; "> Welcome Back  To ICARE</div><br> 
+                <div class="card-header">{{ __('User Profile') }}</div>
                     <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-                        
-                        {{ __('You are logged in!') }}
+                        <table>
+                            <tr colspan="3">
+                                <form action="/updateName" action="POST">
+                                    <td>Username:</td>
+                                    <td><input type="text" name="name" value="{{ Auth::user()->name }}" style="border:none;;background-color:#7C9473;"></td>
+                                    <td><button>Edit Name</button></td>
+                                </form>
+                            </tr>
+                            <tr colspan="3">
+                                <form action="/updatePic" action="POST">
+                                    <td>Profile Picture</td>
+                                    <td><input type="file" name="picture"></td>
+                                    <td><button type="submit">Upload Image</button></td>
+                                </form>
+                            </tr>
+                            <tr colspan="3">
+                                <td>Email</td>
+                                <td>:{{ Auth::user()->email }}</td>
+                                <td><button onclick="location.href='/deleteAccount'">Delete Account</button></td>
+                            </tr>
+                            <tr colspan="3">
+                                <td>Password</td>
+                                <td>:Top Secret</td>
+                                <td><button onclick="location.href='/password/reset'">Change Password</button></td>
+                            </tr>
+                        </table>
                     </div>
                 </div>
             </div>
@@ -44,6 +86,6 @@
         </div>
     </div>
 </div>
-</div> -->
+</div>
 </body>
 @endsection
